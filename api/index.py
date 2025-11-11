0d898be64282189a242ee6daeeb5562b417141d3
Chrisgo05 <A01641386@tec.mx>
2025-11-11 07:40:48 -0600

    Update index.py

--- api/index.py
+++ api/index.py
@@ -31,7 +31,8 @@ def sensor():
         cursor.execute("select * from sensores;")
         result = cursor.fetchall()
         for i in result:
-                print("Current Time:", i, "\n")
+                print("Current Time:", i, """
+                """)
     
         # Close the cursor and connection
         cursor.close()
